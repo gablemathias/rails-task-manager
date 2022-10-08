5f98fddca70b3c5c83027e7a22c5b666c1fe58df
Gabriel Mathias <gablemathias@icloud.com>
2022-10-08 18:32:02 -0300

    add new method

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # resources :tasks
   get 'tasks', to: 'tasks#index'
+
+  get 'tasks/new', to: 'tasks#new'
+
+  get 'tasks/:id', to: 'tasks#show', as: :task
 end
